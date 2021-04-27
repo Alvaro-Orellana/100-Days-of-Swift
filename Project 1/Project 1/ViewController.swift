28d1c169c0e1e1fa1b8589da8679451752036d19
Alvaro <alvher.orellana@gmail.com>
2021-04-26 21:34:07 -0300

    Added first project

---
+++ Project 1/Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Alvaro Orellana on 26-04-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
